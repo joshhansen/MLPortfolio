2dc5a057236e9b9b97c08e80c0ab4830a81ffb5e
Josh Hansen <git@joshhansen.tech>
2023-10-23 21:46:20 -0700

    cuda12_local

--- imdb-sentiment-analysis/jax/init-venv.sh
+++ imdb-sentiment-analysis/jax/init-venv.sh
@@ -8,6 +8,6 @@ PIP="$VENV/bin/pip"
 
 $PIP install --upgrade pip
 
-$PIP install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+$PIP install --upgrade "jax[cuda12_local]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 $PIP install -r other-packages.txt
