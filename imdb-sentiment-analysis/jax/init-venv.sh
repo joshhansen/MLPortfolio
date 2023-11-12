e60eaa93d0a7c555bcfe98b6e9915733440e93a6
Josh Hansen <git@joshhansen.tech>
2023-11-12 13:07:32 -0800

    Try some stuff to get it working through pip

--- imdb-sentiment-analysis/jax/init-venv.sh
+++ imdb-sentiment-analysis/jax/init-venv.sh
@@ -2,12 +2,15 @@
 
 VENV="$PWD/venv"
 
-python -m venv $VENV
+python -m venv --system-site-packages $VENV
 
 PIP="$VENV/bin/pip"
 
 $PIP install --upgrade pip
 
 $PIP install --upgrade "jax[cuda12_local]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# $PIP install --upgrade "jax[cuda11_local]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# $PIP install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# $PIP install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 $PIP install -r other-packages.txt
