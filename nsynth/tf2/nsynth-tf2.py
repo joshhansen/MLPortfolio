c6ca36b1105b7e57c25c65bae453d564e3d5014a
Josh Hansen <git@joshhansen.tech>
2023-11-28 14:09:20 -0800

    Try to fit the model

--- nsynth/tf2/nsynth-tf2.py
+++ nsynth/tf2/nsynth-tf2.py
@@ -6,6 +6,16 @@ import tensorflow as tf
 BATCH_SIZE=100
 SHUFFLE_SIZE=10000
 
+def parse_example(raw):
+ return tf.io.parse_example(raw, {
+  'audio': tf.io.RaggedFeature(dtype=tf.float32),
+  'pitch': tf.io.FixedLenFeature((), tf.int64),
+  'sample_rate': tf.io.FixedLenFeature((), tf.int64),
+ })
+
+def dataset(ds):
+ return ds.map(parse_example).shuffle(SHUFFLE_SIZE).batch(BATCH_SIZE).take(10)
+
 if __name__ == "__main__":
  data_dir = os.path.join(os.environ['HOME'], 'Data/org/tensorflow/magenta')
  train_path = os.path.join(data_dir, 'nsynth-train.tfrecord')
@@ -14,16 +24,16 @@ if __name__ == "__main__":
 
  print(f"train_path: {train_path}")
 
+ train = dataset(tf.data.TFRecordDataset(train_path))
+ val = dataset(tf.data.TFRecordDataset(val_path))
+ test = dataset(tf.data.TFRecordDataset(test_path))
+
  def parse_example(raw):
   return tf.io.parse_example(raw, {
    'audio': tf.io.RaggedFeature(dtype=tf.float32),
    'pitch': tf.io.FixedLenFeature((), tf.int64),
    'sample_rate': tf.io.FixedLenFeature((), tf.int64),
   })
-
- train = tf.data.TFRecordDataset(train_path)
-
- print(train.cardinality())
 
  x = tf.keras.Input((64000,1,), BATCH_SIZE)
  y = x
@@ -44,13 +54,23 @@ if __name__ == "__main__":
 
  #TODO mu-law
 
- for batch in train.map(parse_example).shuffle(SHUFFLE_SIZE).batch(BATCH_SIZE).take(10):
-  print(batch['audio'].shape)
-  print(batch['pitch'])
-  print(batch['sample_rate'])
+ model.compile(
+  optimizer=tf.keras.optimizers.RMSprop(),  # Optimizer
+  # Loss function to minimize
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+  # List of metrics to monitor
+  metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+ )
+
+ # for batch in train.map(parse_example).shuffle(SHUFFLE_SIZE).batch(BATCH_SIZE).take(10):
+ #  print(batch['audio'].shape)
+ #  print(batch['pitch'])
+ #  print(batch['sample_rate'])
 
   
-
-  
-
- 
+ history = model.fit(
+    { 'input_1': train },
+    batch_size=BATCH_SIZE,
+    epochs=2,
+    # validation_data=(val, val),
+ )
