7efc3ab370930e41baa29c39b5bea5b5c225e1be
Josh Hansen <me@joshhansen.tech>
2023-12-01 15:15:49 -0800

    Pick a sensible metric, still broken; decruft

--- nsynth/tf2/nsynth-tf2.py
+++ nsynth/tf2/nsynth-tf2.py
@@ -36,13 +36,6 @@ if __name__ == "__main__":
  val = dataset(val_raw)
  test = dataset(test_raw)
 
- # def parse_example(raw):
- #  return tf.io.parse_example(raw, {
- #   'audio': tf.io.RaggedFeature(dtype=tf.float32),
- #   'pitch': tf.io.FixedLenFeature((), tf.int64),
- #   'sample_rate': tf.io.FixedLenFeature((), tf.int64),
- #  })
-
  x = tf.keras.Input((64000,1,), BATCH_SIZE)
  y = x
 
@@ -65,17 +58,10 @@ if __name__ == "__main__":
 
  model.compile(
   optimizer=tf.keras.optimizers.RMSprop(),  # Optimizer
-  # Loss function to minimize
-  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-  # List of metrics to monitor
-  metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+  loss = tf.keras.losses.MeanSquaredError(),
+  metrics = [tf.keras.metrics.MeanAbsolutePercentageError()]
  )
-
- # for batch in train.map(parse_example).shuffle(SHUFFLE_SIZE).batch(BATCH_SIZE).take(10):
- #  print(batch['audio'].shape)
- #  print(batch['pitch'])
- #  print(batch['sample_rate'])
-  
+ 
  history = model.fit(
     train,
     batch_size=BATCH_SIZE,
