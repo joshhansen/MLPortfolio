b921ff328f321e683274d3d4214ad9cf2228049a
Josh Hansen <me@joshhansen.tech>
2024-10-09 16:20:26 -0700

    Image super-resolution initial training of a single image

---
+++ image-super-resolution/image-sr-train.py
@@ -0,0 +1,196 @@
+from absl import logging
+import flax.linen as nn
+from flax import nnx
+from flax.training.train_state import TrainState
+import imageio
+import jax
+import jax.numpy as jnp
+import optax
+
+
+class Model(nnx.Module):
+    def __init__(self, intermediate_features: int = 16, rngs=nnx.Rngs):
+        self.upscale = nnx.ConvTranspose(
+            in_features=3,
+            out_features=intermediate_features,
+            kernel_size=(4, 4),
+            padding=2,
+            strides=(2, 2),
+            rngs=rngs
+        )
+        self.deepen = nnx.Conv(
+            in_features=intermediate_features,
+            out_features=3,
+            kernel_size=(3, 3),
+            padding=1,
+            rngs=rngs
+        )
+
+    def __call__(self, x: jax.Array):
+        return self.deepen(self.upscale(x))
+
+
+# @dataclass
+# class Params:
+#     convs: list[nn.Conv]
+
+#     @classmethod
+#     def initialize(cls, layers: int = 8, features: int = 16):
+#         convs = list()
+
+#         for _ in range(layers):
+#             convs.append(nn.Conv(features=features,
+#                          kernel_size=(3,), padding='VALID', strides=2, kernel_dilation=2))
+
+#         return cls(convs)
+#         # emb_key, attn_key, attn_query_key, linear1_key, linear2_key = jrand.split(key, 5)
+#         # emb =  initializer(emb_key, (vocab_len, EMBEDDING_DIMS), dtype=fX)
+#         # attn =  MultiheadAttentionParams.initialize(attn_key, initializer, ATTN_HEADS, EMBEDDING_DIMS, ATTN_DIMS_PER_HEAD, EMBEDDING_DIMS)
+#         # attn_query = initializer(attn_query_key, (target_len, EMBEDDING_DIMS), dtype=fX)
+#         # linear1 =  Linear.initialize(linear1_key, MODEL_DIMS, MODEL_DIMS // 2, fX)
+#         # linear2 =  Linear.initialize(linear2_key, MODEL_DIMS // 2, 1, fX)
+
+#         # return cls(emb, attn, attn_query, linear1, linear2)
+
+
+# def model(params: Params, full: jax.Array, small: jax.Array):
+#     pass
+
+
+# model_grad = jax.grad(model)
+
+
+@jax.jit
+def apply_model(state: TrainState, X: jax.Array, Y: jax.Array):
+    """Computes gradients, loss and accuracy for a single batch."""
+
+    def loss_fn(params):
+        preds = state.apply_fn(params, X)
+        loss = jnp.mean(optax.squared_error(preds, Y))
+        return loss, preds
+
+    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+    (loss, preds), grads = grad_fn(state.params)
+    return grads, loss
+
+
+@jax.jit
+def update_model(state: TrainState, grads):
+    return state.apply_gradients(grads=grads)
+
+
+def train_epoch(
+    state: TrainState,
+    X: jax.Array,
+    Y: jax.Array,
+    rng: jax.Array
+):
+    # """Train for a single epoch."""
+    # train_ds_size = len(train_ds['image'])
+    # steps_per_epoch = train_ds_size // batch_size
+
+    # perms = jax.random.permutation(rng, len(train_ds['image']))
+    # perms = perms[: steps_per_epoch * batch_size]  # skip incomplete batch
+    # perms = perms.reshape((steps_per_epoch, batch_size))
+
+    # epoch_loss = []
+    # epoch_accuracy = []
+
+    # for perm in perms:
+    #   batch_images = train_ds['image'][perm, ...]
+    #   batch_labels = train_ds['label'][perm, ...]
+    #   grads, loss, accuracy = apply_model(state, batch_images, batch_labels)
+    #   state = update_model(state, grads)
+    #   epoch_loss.append(loss)
+    #   epoch_accuracy.append(accuracy)
+    # train_loss = np.mean(epoch_loss)
+    # train_accuracy = np.mean(epoch_accuracy)
+    # return state, train_loss, train_accuracy
+
+    # epoch_loss = list()
+
+    grads, loss = apply_model(state, X, Y)
+    state = update_model(state, grads)
+    # epoch_loss.append(loss)
+
+    # train_loss = jnp.mean(epoch_loss)
+
+    # return state, train_loss
+    return state, loss
+
+
+@nnx.jit
+def train_step(
+    m: Model,
+    opt: nnx.Optimizer,
+    small: jax.Array,
+    full: jax.Array
+):
+    def loss_fn(m: Model):
+        pred = m(small)
+        return jnp.mean(optax.squared_error(pred, full))
+
+    loss, grads = nnx.value_and_grad(loss_fn)(m)
+    opt.update(grads)  # in-place updates
+
+    return loss
+
+
+if __name__ == "__main__":
+    full_np = imageio.v3.imread('/home/josh/Data/image.jpg')
+    small_np = imageio.v3.imread('/home/josh/Data/image-small.jpg')
+
+    full: jax.Array = jnp.asarray(
+        full_np, dtype='float32').reshape(1, *full_np.shape)
+    small: jax.Array = jnp.asarray(
+        small_np, dtype='float32').reshape(1, *small_np.shape)
+
+    print(f"Full shape: {full.shape}")
+    print(f"Small shape: {small.shape}")
+
+    # params = Params.initialize()
+
+    # dmodel = model_grad(params, full, small)
+
+    # print(dmodel)
+
+    LR = 0.001
+    MOMENTUM = 0.1
+    ITS = 10
+
+    rngs = nnx.Rngs(98239)
+
+    m = Model(rngs=rngs)
+    opt = nnx.Optimizer(m, optax.adam(1e-3))
+    # key = jax.random.key(0)
+    # params = m.init(key, jnp.zeros((1024, 1024, 3)))
+    # tx = optax.sgd(LR, MOMENTUM)
+    # state = TrainState.create(
+    # apply_fn=m.apply, params=params, tx=tx)
+
+    # workdir = "/tmp/image-sr"
+    # summary_writer = SummaryWriter(workdir)
+    # summary_writer.hparams(dict(config))
+
+    for epoch in range(ITS):
+        train_loss = train_step(m, opt, small, full)
+        test_loss = -1
+        # key, input_key = jax.random.split(key)
+        # state, train_loss = train_epoch(
+        #     state, small, full, key
+        # )
+        # _, test_loss = apply_model(
+        #     state, small, full
+        # )
+
+        print(
+            'epoch:% 3d, train_loss: %.4f, test_loss: %.4f,'
+            % (
+                epoch,
+                train_loss,
+                test_loss,
+            )
+        )
+
+        # summary_writer.scalar('train_loss', train_loss, epoch)
+        # summary_writer.scalar('test_loss', test_loss, epoch)
