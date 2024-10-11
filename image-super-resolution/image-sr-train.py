477d51e452b5f8d084cb6c05356f1acd0f47f04e
Josh Hansen <me@joshhansen.tech>
2024-10-11 15:10:39 -0700

    Deal with odd-sized dimensions

--- image-super-resolution/image-sr-train.py
+++ image-super-resolution/image-sr-train.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
+import sys
 
-from absl import logging
-import flax.linen as nn
+import flax
 from flax import nnx
 from flax.training.train_state import TrainState
 import imageio
@@ -32,36 +33,6 @@ class Model(nnx.Module):
         return self.deepen(self.upscale(x))
 
 
-# @dataclass
-# class Params:
-#     convs: list[nn.Conv]
-
-#     @classmethod
-#     def initialize(cls, layers: int = 8, features: int = 16):
-#         convs = list()
-
-#         for _ in range(layers):
-#             convs.append(nn.Conv(features=features,
-#                          kernel_size=(3,), padding='VALID', strides=2, kernel_dilation=2))
-
-#         return cls(convs)
-#         # emb_key, attn_key, attn_query_key, linear1_key, linear2_key = jrand.split(key, 5)
-#         # emb =  initializer(emb_key, (vocab_len, EMBEDDING_DIMS), dtype=fX)
-#         # attn =  MultiheadAttentionParams.initialize(attn_key, initializer, ATTN_HEADS, EMBEDDING_DIMS, ATTN_DIMS_PER_HEAD, EMBEDDING_DIMS)
-#         # attn_query = initializer(attn_query_key, (target_len, EMBEDDING_DIMS), dtype=fX)
-#         # linear1 =  Linear.initialize(linear1_key, MODEL_DIMS, MODEL_DIMS // 2, fX)
-#         # linear2 =  Linear.initialize(linear2_key, MODEL_DIMS // 2, 1, fX)
-
-#         # return cls(emb, attn, attn_query, linear1, linear2)
-
-
-# def model(params: Params, full: jax.Array, small: jax.Array):
-#     pass
-
-
-# model_grad = jax.grad(model)
-
-
 @jax.jit
 def apply_model(state: TrainState, X: jax.Array, Y: jax.Array):
     """Computes gradients, loss and accuracy for a single batch."""
@@ -87,38 +58,15 @@ def train_epoch(
     Y: jax.Array,
     rng: jax.Array
 ):
-    # """Train for a single epoch."""
-    # train_ds_size = len(train_ds['image'])
-    # steps_per_epoch = train_ds_size // batch_size
-
-    # perms = jax.random.permutation(rng, len(train_ds['image']))
-    # perms = perms[: steps_per_epoch * batch_size]  # skip incomplete batch
-    # perms = perms.reshape((steps_per_epoch, batch_size))
-
-    # epoch_loss = []
-    # epoch_accuracy = []
-
-    # for perm in perms:
-    #   batch_images = train_ds['image'][perm, ...]
-    #   batch_labels = train_ds['label'][perm, ...]
-    #   grads, loss, accuracy = apply_model(state, batch_images, batch_labels)
-    #   state = update_model(state, grads)
-    #   epoch_loss.append(loss)
-    #   epoch_accuracy.append(accuracy)
-    # train_loss = np.mean(epoch_loss)
-    # train_accuracy = np.mean(epoch_accuracy)
-    # return state, train_loss, train_accuracy
-
-    # epoch_loss = list()
-
     grads, loss = apply_model(state, X, Y)
     state = update_model(state, grads)
-    # epoch_loss.append(loss)
 
-    # train_loss = jnp.mean(epoch_loss)
-
-    # return state, train_loss
     return state, loss
+
+
+@jax.jit
+def loss(pred: jax.Array, full: jax.Array) -> jax.Array:
+    return jnp.mean(optax.squared_error(pred, full))
 
 
 @nnx.jit
@@ -130,12 +78,22 @@ def train_step(
 ):
     def loss_fn(m: Model):
         pred = m(small)
-        return jnp.mean(optax.squared_error(pred, full))
+        return loss(pred, full)
 
-    loss, grads = nnx.value_and_grad(loss_fn)(m)
+    l, grads = nnx.value_and_grad(loss_fn)(m)
     opt.update(grads)  # in-place updates
 
-    return loss
+    return l
+
+
+def is_testing(filename: str) -> bool:
+    basename = Path(filename).stem
+    return basename.endswith('0')
+
+
+def is_valid(filename: str) -> bool:
+    basename = Path(filename).stem
+    return basename.endswith('1')
 
 
 if __name__ == "__main__":
@@ -144,6 +102,9 @@ if __name__ == "__main__":
         os.environ['HOME'], "Data/com/github/cvdfoundation/google-landmark")
     SMALL_DIR = os.path.join(DATA_DIR, "train_downsampled")
     FULL_DIR = os.path.join(DATA_DIR, "train")
+
+    print(f"DATA_DIR: {DATA_DIR}")
+    sys.stdout.flush()
 
     LR = 0.001
     MOMENTUM = 0.1
@@ -155,42 +116,95 @@ if __name__ == "__main__":
     opt = nnx.Optimizer(m, optax.adam(1e-3))
 
     for epoch in range(ITS):
-        count = 0
+        train_count = 0
+        test_count = 0
         total_train_loss = 0.0
+        total_test_loss = 0.0
         for dirpath, _dirnames, filenames in os.walk(SMALL_DIR):
             reldirpath = os.path.relpath(dirpath, SMALL_DIR)
             fulldirpath = os.path.join(FULL_DIR, reldirpath)
-            print(f"dirpath: {dirpath}")
-            print(reldirpath)
-            print(fulldirpath)
 
             for filename in filenames:
+                testing = is_testing(filename)
+                valid = is_valid(filename)
+
                 small_path = os.path.join(dirpath, filename)
                 full_path = os.path.join(fulldirpath, filename)
 
-                small_np = imageio.v3.imread(small_path)
-                full_np = imageio.v3.imread(full_path)
+                print(small_path)
+
+                small_np = imageio.v3.imread(small_path, mode="RGB")
+                full_np = imageio.v3.imread(full_path, mode="RGB")
 
                 small: jax.Array = jnp.asarray(
                     small_np, dtype='float32').reshape(1, *small_np.shape)
                 full: jax.Array = jnp.asarray(
                     full_np, dtype='float32').reshape(1, *full_np.shape)
 
-                print(f"Small shape: {small.shape}")
-                print(f"Full shape: {full.shape}")
+                # print(f"Small shape: {small.shape}")
+                # print(f"Full shape: {full.shape}")
 
-                count += 1
-                total_train_loss += train_step(m, opt, small, full)
-                test_loss = -1
+                sys.stdout.flush()
 
-                if count % 10 == 0:
+                small_new_shape = list(small.shape)
+                full_new_shape = list(full.shape)
+                resize_small = False
+                resize_full = False
+                for dim in (1, 2):
+                    assert full_new_shape[dim] > small_new_shape[dim]
 
-                    epoch_avg_train_loss = total_train_loss / count
+                    # Make sure the larger dim is even
+                    if full_new_shape[dim] % 2 == 1:
+                        resize_full = True
+                        full_new_shape[dim] -= 1
+
+                    assert full_new_shape[dim] % 2 == 0
+
+                    # Decrease the smaller dim until 2*smaller = larger
+                    while small_new_shape[dim] * 2 > full_new_shape[dim]:
+                        resize_small = True
+                        small_new_shape[dim] -= 1
+
+                    assert small_new_shape[dim] * 2 == full_new_shape[dim]
+
+                if resize_small:
+                    small = jnp.resize(small, tuple(small_new_shape))
+
+                if resize_full:
+                    full = jnp.resize(full, tuple(full_new_shape))
+
+                # print(f"small_new_shape: {small_new_shape}")
+                # print(f"full_new_shape: {full_new_shape}")
+
+                if testing:
+                    test_count += 1
+                    pred = m(small)
+                    total_test_loss += loss(pred, full)
+                elif not valid:
+                    train_count += 1
+                    total_train_loss += train_step(m, opt, small, full)
+
+                if train_count % 10 == 0 and train_count > 0:
+                    epoch_avg_train_loss = total_train_loss / train_count
+                    try:
+                        epoch_avg_test_loss = total_test_loss / test_count
+                    except ZeroDivisionError:
+                        epoch_avg_test_loss = float('nan')
+
                     print(
-                        'epoch:% 3d, epoch avg train loss: %.4f, test_loss: %.4f,'
+                        'epoch:% 3d, train_count: %d, epoch avg train loss: %.4f, epoch avg test_loss: %.4f,'
                         % (
                             epoch,
+                            train_count,
                             epoch_avg_train_loss,
-                            test_loss,
+                            epoch_avg_test_loss,
                         )
                     )
+
+                    sys.stdout.flush()
+
+                    state = nnx.state(m)
+                    ser = flax.serialization.to_bytes(state)
+
+                    with open('./model.ser', 'wb') as w:
+                        w.write(ser)
