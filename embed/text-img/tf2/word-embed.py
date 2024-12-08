228fece6c395aa9f4fdacaa498e84b0c6291db56
Josh Hansen <me@joshhansen.tech>
2024-12-07 16:23:09 -0800

    Word embedding work; copy example transformer

--- embed/text-img/tf2/word-embed.py
+++ embed/text-img/tf2/word-embed.py
@@ -34,17 +34,324 @@ def index_batch(grapheme_idx: T2I, batched_token_graphemes: list[list[str]]):
  print(f"padded: {padded}")
  return padded, max_len
 
+class PositionalEmbedding(tf.keras.layers.Layer):
+ def __init__(self, vocab_size, d_model):
+  super().__init__()
+  self.d_model = d_model
+  self.embedding = tf.keras.layers.Embedding(vocab_size, d_model, mask_zero=True) 
+  self.pos_encoding = positional_encoding(length=2048, depth=d_model)
+
+ def compute_mask(self, *args, **kwargs):
+  return self.embedding.compute_mask(*args, **kwargs)
+
+ def call(self, x):
+  length = tf.shape(x)[1]
+  x = self.embedding(x)
+  # This factor sets the relative scale of the embedding and positonal_encoding.
+  x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+  x = x + self.pos_encoding[tf.newaxis, :length, :]
+  return x
+
+class BaseAttention(tf.keras.layers.Layer):
+ def __init__(self, **kwargs):
+  super().__init__()
+  self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+  self.layernorm = tf.keras.layers.LayerNormalization()
+  self.add = tf.keras.layers.Add()
+
+class CrossAttention(BaseAttention):
+ def call(self, x, context):
+  attn_output, attn_scores = self.mha(
+      query=x,
+      key=context,
+      value=context,
+      return_attention_scores=True)
+
+  # Cache the attention scores for plotting later.
+  self.last_attn_scores = attn_scores
+
+  x = self.add([x, attn_output])
+  x = self.layernorm(x)
+
+  return x
+
+class GlobalSelfAttention(BaseAttention):
+ def call(self, x):
+  attn_output = self.mha(
+      query=x,
+      value=x,
+      key=x)
+  x = self.add([x, attn_output])
+  x = self.layernorm(x)
+  return x
+
+class CausalSelfAttention(BaseAttention):
+ def call(self, x):
+  attn_output = self.mha(
+      query=x,
+      value=x,
+      key=x,
+      use_causal_mask = True)
+  x = self.add([x, attn_output])
+  x = self.layernorm(x)
+  return x
+
+class FeedForward(tf.keras.layers.Layer):
+ def __init__(self, d_model: int, dff: int, dropout_rate=0.1):
+  super().__init__()
+  self.seq = tf.keras.Sequential([
+    tf.keras.layers.Dense(dff, activation='relu'),
+    tf.keras.layers.Dense(d_model),
+    tf.keras.layers.Dropout(dropout_rate)
+  ])
+  self.add = tf.keras.layers.Add()
+  self.layer_norm = tf.keras.layers.LayerNormalization()
+
+ def call(self, x):
+  x = self.add([x, self.seq(x)])
+  x = self.layer_norm(x) 
+  return x
+
+class EncoderLayer(tf.keras.layers.Layer):
+ def __init__(self,*, d_model: int, num_heads: int, dff: int, dropout_rate=0.1):
+  super().__init__()
+
+  self.self_attention = GlobalSelfAttention(
+      num_heads=num_heads,
+      key_dim=d_model,
+      dropout=dropout_rate)
+
+  self.ffn = FeedForward(d_model, dff)
+
+ def call(self, x):
+  x = self.self_attention(x)
+  x = self.ffn(x)
+  return x
+
+class Encoder(tf.keras.layers.Layer):
+ def __init__(self, *, num_layers: int, d_model: int, num_heads: int,
+             dff: int, vocab_size: int, dropout_rate=0.1):
+  super().__init__()
+
+  self.d_model = d_model
+  self.num_layers = num_layers
+
+  self.pos_embedding = PositionalEmbedding(
+      vocab_size=vocab_size, d_model=d_model)
+
+  self.enc_layers = [
+      EncoderLayer(d_model=d_model,
+                   num_heads=num_heads,
+                   dff=dff,
+                   dropout_rate=dropout_rate)
+      for _ in range(num_layers)]
+  self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+ def call(self, x):
+  # `x` is token-IDs shape: (batch, seq_len)
+  x = self.pos_embedding(x)  # Shape `(batch_size, seq_len, d_model)`.
+
+  # Add dropout.
+  x = self.dropout(x)
+
+  for i in range(self.num_layers):
+    x = self.enc_layers[i](x)
+
+  return x  # Shape `(batch_size, seq_len, d_model)`.
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+ def __init__(self,
+             *,
+             d_model: int,
+             num_heads: int,
+             dff: int,
+             dropout_rate=0.1):
+  super(DecoderLayer, self).__init__()
+
+  self.causal_self_attention = CausalSelfAttention(
+      num_heads=num_heads,
+      key_dim=d_model,
+      dropout=dropout_rate)
+
+  self.cross_attention = CrossAttention(
+      num_heads=num_heads,
+      key_dim=d_model,
+      dropout=dropout_rate)
+
+  self.ffn = FeedForward(d_model, dff)
+
+ def call(self, x, context):
+  x = self.causal_self_attention(x=x)
+  x = self.cross_attention(x=x, context=context)
+
+  # Cache the last attention scores for plotting later
+  self.last_attn_scores = self.cross_attention.last_attn_scores
+
+  x = self.ffn(x)  # Shape `(batch_size, seq_len, d_model)`.
+  return x 
+
+class DecoderOld(tf.keras.layers.Layer):
+  @classmethod
+  def add_method(cls, fun):
+    setattr(cls, fun.__name__, fun)
+    return fun
+
+  def __init__(self, grapheme_count: int, units: int):
+    # super(Decoder, self).__init__()
+    # self.text_processor = text_processor
+    # self.vocab_size = text_processor.vocabulary_size()
+    # self.word_to_id = tf.keras.layers.StringLookup(
+    #     vocabulary=text_processor.get_vocabulary(),
+    #     mask_token='', oov_token='[UNK]')
+    # self.id_to_word = tf.keras.layers.StringLookup(
+    #     vocabulary=text_processor.get_vocabulary(),
+    #     mask_token='', oov_token='[UNK]',
+    #     invert=True)
+    # self.start_token = self.word_to_id('[START]')
+    # self.end_token = self.word_to_id('[END]')
+
+    # self.units = units
+
+
+    # # 1. The embedding layer converts token IDs to vectors
+    # self.embedding = tf.keras.layers.Embedding(self.vocab_size,
+    #                                            units, mask_zero=True)
+
+    # 2. The RNN keeps track of what's been generated so far.
+    self.rnn = tf.keras.layers.GRU(units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+    # 3. The RNN output will be the query for the attention layer.
+    self.attention = CrossAttention(units)
+
+    # 4. This fully connected layer produces the logits for each
+    # output grapheme.
+    self.output_layer = tf.keras.layers.Dense(grapheme_count)
+
+    @Decoder.add_method
+    def call(self,
+             token_emb,
+             state=None,
+             return_state=False):  
+      # shape_checker = ShapeChecker()
+      # shape_checker(x, 'batch t')
+      # shape_checker(context, 'batch s units')
+
+      # # 1. Lookup the embeddings
+      # x = self.embedding(x)
+      # shape_checker(x, 'batch t units')
+
+      # 2. Process the target sequence.
+      x, state = self.rnn(token_emb)
+      shape_checker(x, 'batch t units')
+
+      # 3. Use the RNN output as the query for the attention over the context.
+      x = self.attention(x, context)
+      self.last_attention_weights = self.attention.last_attention_weights
+      # shape_checker(x, 'batch t units')
+      # shape_checker(self.last_attention_weights, 'batch t s')
+
+      # Step 4. Generate logit predictions for the next token.
+      logits = self.output_layer(x)
+      shape_checker(logits, 'batch t target_vocab_size')
+
+      if return_state:
+        return logits, state
+      else:
+        return logits
+
+class Decoder(tf.keras.layers.Layer):
+ def __init__(self, *, num_layers: int, d_model: int, num_heads: int, dff: int,
+              vocab_size: int, dropout_rate=0.1):
+  super(Decoder, self).__init__()
+
+  self.d_model = d_model
+  self.num_layers = num_layers
+
+  self.pos_embedding = PositionalEmbedding(vocab_size=vocab_size,
+                                           d_model=d_model)
+  self.dropout = tf.keras.layers.Dropout(dropout_rate)
+  self.dec_layers = [
+      DecoderLayer(d_model=d_model, num_heads=num_heads,
+                   dff=dff, dropout_rate=dropout_rate)
+      for _ in range(num_layers)]
+
+  self.last_attn_scores = None
+
+ def call(self, x, context):
+  # `x` is token-IDs shape (batch, target_seq_len)
+  x = self.pos_embedding(x)  # (batch_size, target_seq_len, d_model)
+
+  x = self.dropout(x)
+
+  for i in range(self.num_layers):
+    x  = self.dec_layers[i](x, context)
+
+  self.last_attn_scores = self.dec_layers[-1].last_attn_scores
+
+  # The shape of x is (batch_size, target_seq_len, d_model).
+  return x
+
+class Transformer(tf.keras.Model):
+ def __init__(self, *, num_layers: int, d_model: int, num_heads: int, dff: int,
+             input_vocab_size: int, target_vocab_size: int, dropout_rate=0.1):
+  super().__init__()
+  self.encoder = Encoder(num_layers=num_layers, d_model=d_model,
+                         num_heads=num_heads, dff=dff,
+                         vocab_size=input_vocab_size,
+                         dropout_rate=dropout_rate)
+
+  self.decoder = Decoder(num_layers=num_layers, d_model=d_model,
+                         num_heads=num_heads, dff=dff,
+                         vocab_size=target_vocab_size,
+                         dropout_rate=dropout_rate)
+
+  self.final_layer = tf.keras.layers.Dense(target_vocab_size)
+
+ def call(self, inputs):
+  # To use a Keras model with `.fit` you must pass all your inputs in the
+  # first argument.
+  context, x  = inputs
+
+  context = self.encoder(context)  # (batch_size, context_len, d_model)
+
+  x = self.decoder(x, context)  # (batch_size, target_len, d_model)
+
+  # Final linear layer output.
+  logits = self.final_layer(x)  # (batch_size, target_len, target_vocab_size)
+
+  try:
+    # Drop the keras mask, so it doesn't scale the losses/metrics.
+    # b/250038731
+    del logits._keras_mask
+  except AttributeError:
+    pass
+
+  # Return the final output and the attention weights.
+  return logits
+
+
 class WordAutoencoder(tf.keras.layers.Layer):
     def __init__(self, grapheme_count: int):
         super().__init__()
 
-        self.grapheme_query = self.add_weight(shape=(BATCH, GRAPHEME_QUERY, EMBED), initializer="random_normal", trainable=True)
+        self.transformer = Transformer(
+         num_layers: int, d_model: int, num_heads: int, dff: int,
+         input_vocab_size: int, target_vocab_size: int, dropout_rate=0.1
+        )
+
+        # self.grapheme_enc_query = self.add_weight(shape=(BATCH, GRAPHEME_QUERY, EMBED), initializer="random_normal", trainable=True)
+        # self.grapheme_dec_query = self.add_weight(shape=(BATCH, GRAPHEME_QUERY, grapheme_count), initializer="random_normal", trainable=True)
         self.grapheme_emb = tf.keras.layers.Embedding(
          grapheme_count,
          EMBED,
         )
-        self.grapheme_attn = tf.keras.layers.Attention()
-
+        # self.grapheme_attn_enc = tf.keras.layers.Attention()
+        # self.grapheme_attn_dec = tf.keras.layers.Attention()
+        # self.grapheme_attn = tf.keras.layers.MultiHeadAttention(key_dim=EMBED, num_heads=1)
 
     # token_grapheme_indices: (batch, token, grapheme); integers representing token indices 
     def call(self, token_grapheme_indices: tf.Tensor):
@@ -54,12 +361,73 @@ class WordAutoencoder(tf.keras.layers.Layer):
      grapheme_embs = self.grapheme_emb(grapheme_indices)
      print(grapheme_embs)
 
-     token_emb = self.grapheme_attn([self.grapheme_query, grapheme_embs])
+     # token_emb = self.grapheme_attn([self.grapheme_query, grapheme_embs])
+     # token_emb = self.grapheme_attn(query=self.grapheme_query, value=grapheme_embs)
 
      #TODO decoder
 
+     # prior = tf.zeros((BATCH, ))
+     # while True:
+
+     output = self.transformer(grapheme_embs)
+
      return token_emb
 
+class Translator(tf.Module):
+  def __init__(self, tokenizers, transformer):
+    self.tokenizers = tokenizers
+    self.transformer = transformer
+
+  def __call__(self, sentence, max_length=MAX_TOKENS):
+    # The input sentence is Portuguese, hence adding the `[START]` and `[END]` tokens.
+    assert isinstance(sentence, tf.Tensor)
+    if len(sentence.shape) == 0:
+      sentence = sentence[tf.newaxis]
+
+    sentence = self.tokenizers.pt.tokenize(sentence).to_tensor()
+
+    encoder_input = sentence
+
+    # As the output language is English, initialize the output with the
+    # English `[START]` token.
+    start_end = self.tokenizers.en.tokenize([''])[0]
+    start = start_end[0][tf.newaxis]
+    end = start_end[1][tf.newaxis]
+
+    # `tf.TensorArray` is required here (instead of a Python list), so that the
+    # dynamic-loop can be traced by `tf.function`.
+    output_array = tf.TensorArray(dtype=tf.int64, size=0, dynamic_size=True)
+    output_array = output_array.write(0, start)
+
+    for i in tf.range(max_length):
+      output = tf.transpose(output_array.stack())
+      predictions = self.transformer([encoder_input, output], training=False)
+
+      # Select the last token from the `seq_len` dimension.
+      predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
+
+      predicted_id = tf.argmax(predictions, axis=-1)
+
+      # Concatenate the `predicted_id` to the output which is given to the
+      # decoder as its input.
+      output_array = output_array.write(i+1, predicted_id[0])
+
+      if predicted_id == end:
+        break
+
+    output = tf.transpose(output_array.stack())
+    # The output shape is `(1, tokens)`.
+    text = tokenizers.en.detokenize(output)[0]  # Shape: `()`.
+
+    tokens = tokenizers.en.lookup(output)[0]
+
+    # `tf.function` prevents us from using the attention_weights that were
+    # calculated on the last iteration of the loop.
+    # So, recalculate them outside the loop.
+    self.transformer([encoder_input, output[:,:-1]], training=False)
+    attention_weights = self.transformer.decoder.last_attn_scores
+
+    return text, tokens, attention_weights
 
 if __name__=="__main__":
  with tf.device('/CPU:0'):
@@ -96,14 +464,31 @@ if __name__=="__main__":
   text = WikipediaTitlesDataset(wptitles_path)
   text = text.map(lambda x: tokenizer.tokenize(x))
 
+  num_layers = 4
+  d_model = 128
+  dff = 512
+  num_heads = 8
+  dropout_rate = 0.1
 
-  m = WordAutoencoder(grapheme_idx.t2i.highest_idx + 1)
+  grapheme_count = grapheme_idx.t2i.highest_idx + 1
+
+  transformer = Transformer(
+    num_layers=num_layers,
+    d_model=d_model,
+    num_heads=num_heads,
+    dff=dff,
+    input_vocab_size=grapheme_count ,
+    target_vocab_size=grapheme_count ,
+    dropout_rate=dropout_rate)
+
+  # m = WordAutoencoder(grapheme_idx.t2i.highest_idx + 1)
  # model.compile(optimizer='adam',
  #   loss='sparse_categorical_crossentropy',
  #   metrics=['accuracy'])
  # model.fit(x_train, y_train, epochs=5)
  # model.evaluate(x_test, y_test)
 
+  m = Translator(transformer)
 
 
 
