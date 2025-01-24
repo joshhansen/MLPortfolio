ed1ea29da63639da3ecaf14d63161bcb065cac48
Josh Hansen <me@joshhansen.tech>
2025-01-24 13:43:32 -0800

    Implement custom training loop
    Trains all components (in theory)

--- embed/text-img/tf2/text-img-emb-train.py
+++ embed/text-img/tf2/text-img-emb-train.py
@@ -1,7 +1,10 @@
+from statistics import fmean
+
 from img_enc_dec import Encoder as ImgEncoder, Decoder as ImgDecoder, EncDec as ImgEncDec, load_datasets as load_img_datasets, W, H
 from text_enc_dec import Encoder as TextEncoder, Decoder as TextDecoder, EncDec as TextEncDec, MAX_STR_LEN, prepare_batch as prepare_text_batch, load_datasets as load_text_datasets
 
 import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
 
 EMB=128
 
@@ -158,6 +161,29 @@ class ImgBinOp(tf.keras.layers.Layer):
 
   return z
 
+# Capture a relationship of isomorphism between two groups
+#
+# The dataset should provide (a,b), c with c = a*b
+#
+# So the binary operation on the source group is given; we are training the src->dest and dest->src mappings and the binary operation in the destination
+class BinOpRoundTrip(tf.keras.layers.Layer):
+ def __init__(self, *, src_to_dest: tf.keras.layers.Layer, dest_to_src: tf.keras.layers.Layer, dest_bin_op: tf.keras.layers.Layer):
+  self.src_to_dest = src_to_test
+  self.dest_to_src = dest_to_src
+  self.dest_bin_op = dest_bin_op
+
+ # The dataset should have (a,b) features and c labels, where a * b = c captures binary operation * on the source domain
+ def call(self, inputs: tuple[tf.Tensor, tf.Tensor]):
+  src_a, src_b = inputs
+
+  dest_a = self.src_to_dest(src_a)
+  dest_b = self.src_to_dest(src_b)
+  dest_c = self.dest_bin_op(dest_a, dest_b)
+
+  pred_src_c = self.dest_to_src(dest_c)
+
+  return pred_src_c
+
 class RoundTripBase(tf.keras.layers.Layer):
   def __init__(self, *, img_enc: ImgEncoder, img_dec: ImgDecoder, text_enc: TextEncoder, text_dec: TextDecoder):
     super().__init__()
@@ -222,6 +248,7 @@ class EmbeddingRoundTripper(tf.keras.Model):
     return img0_pred, text0_pred, img1_pred, text1_pred
     
 
+  
 @tf.function
 def train_step(real_images):
     # Sample random points in the latent space
@@ -259,62 +286,442 @@ def train_step(real_images):
     g_optimizer.apply_gradients(zip(grads, generator.trainable_weights))
     return d_loss, g_loss, generated_images
 
-def train():
-  import time
+# def train():
+#   import time
 
-  epochs = 2
-  for epoch in range(epochs):
-      print("\nStart of epoch %d" % (epoch,))
-      start_time = time.time()
+#   epochs = 2
+#   for epoch in range(epochs):
+#       print("\nStart of epoch %d" % (epoch,))
+#       start_time = time.time()
 
-      # Iterate over the batches of the dataset.
-      for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
-          loss_value = train_step(x_batch_train, y_batch_train)
+#       # Iterate over the batches of the dataset.
+#       for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+#           loss_value = train_step(x_batch_train, y_batch_train)
 
-          # Log every 200 batches.
-          if step % 200 == 0:
-              print(
-                  "Training loss (for one batch) at step %d: %.4f"
-                  % (step, float(loss_value))
-              )
-              print("Seen so far: %d samples" % ((step + 1) * batch_size))
+#           # Log every 200 batches.
+#           if step % 200 == 0:
+#               print(
+#                   "Training loss (for one batch) at step %d: %.4f"
+#                   % (step, float(loss_value))
+#               )
+#               print("Seen so far: %d samples" % ((step + 1) * batch_size))
 
-      # Display metrics at the end of each epoch.
-      train_acc = train_acc_metric.result()
-      print("Training acc over epoch: %.4f" % (float(train_acc),))
+#       # Display metrics at the end of each epoch.
+#       train_acc = train_acc_metric.result()
+#       print("Training acc over epoch: %.4f" % (float(train_acc),))
 
-      # Reset training metrics at the end of each epoch
-      train_acc_metric.reset_states()
+#       # Reset training metrics at the end of each epoch
+#       train_acc_metric.reset_states()
 
-      # Run a validation loop at the end of each epoch.
-      for x_batch_val, y_batch_val in val_dataset:
-          test_step(x_batch_val, y_batch_val)
+#       # Run a validation loop at the end of each epoch.
+#       for x_batch_val, y_batch_val in val_dataset:
+#           test_step(x_batch_val, y_batch_val)
 
-      val_acc = val_acc_metric.result()
-      val_acc_metric.reset_states()
-      print("Validation acc: %.4f" % (float(val_acc),))
-      print("Time taken: %.2fs" % (time.time() - start_time))
+#       val_acc = val_acc_metric.result()
+#       val_acc_metric.reset_states()
+#       print("Validation acc: %.4f" % (float(val_acc),))
+#       print("Time taken: %.2fs" % (time.time() - start_time))
+
+# Image autoencoder training step
+#
+# x shape: (batch, w, h, c)
+@tf.function
+def img_enc_dec_train_step(*,
+ opt: tf.keras.optimizers.Optimizer,
+ loss_fn: tf.keras.Loss,
+ img_enc: ImgEncoder,
+ img_dec: ImgDecoder,
+ x: tf.Tensor,
+ loss_fn = tf.keras.losses.MeanSquaredError(),
+ train = True,
+):
+ with tf.GradientTape as tape:
+  y = img_dec(img_enc(x))
+  loss = loss_fn(x, y)
+
+ if train:
+  enc_grads = tape.gradient(loss, img_enc.trainable_weights)
+  dec_grads = tape.gradient(loss, img_dec.trainable_weights)
+  opt.apply_gradients(zip(enc_grads, img_enc.trainable_weights))
+  opt.apply_gradients(zip(dec_grads, img_dec.trainable_weights))
+
+ return loss.numpy()
+ 
+# Text autoencoder training step
+#
+# x shape: (batch, seq)
+@tf.function
+def txt_enc_dec_train_step(*,
+ opt: tf.keras.optimizers.Optimizer,
+ txt_enc: TextEncoder,
+ txt_dec: TextDecoder,
+ x: tf.Tensor,
+ loss_fn = tf.keras.losses.SparseCategoricalCrossEntropy(),
+ train = True,
+):
+ with tf.GradientTape as tape:
+  y = txt_dec(txt_enc(x))
+  loss = loss_fn(x, y)
+
+ if train:
+  enc_grads = tape.gradient(loss, txt_enc.trainable_weights)
+  dec_grads = tape.gradient(loss, txt_dec.trainable_weights)
+  opt.apply_gradients(zip(enc_grads, txt_enc.trainable_weights))
+  opt.apply_gradients(zip(dec_grads, txt_dec.trainable_weights))
+
+ return loss.numpy()
+
+# Image autoencoder training step, starting from embedding
+#
+# x shape: (batch, emb)
+@tf.function
+def img_dec_enc_train_step(*,
+ opt: tf.keras.optimizers.Optimizer,
+ img_enc: ImgEncoder,
+ img_dec: ImgDecoder,
+ x: tf.Tensor,
+ loss_fn = tf.keras.losses.MeanSquaredError(),
+ train = True,
+):
+ with tf.GradientTape as tape:
+  y = img_enc(img_dec(x))
+  loss = loss_fn(x, y)
+
+ if train:
+  enc_grads = tape.gradient(loss, img_enc.trainable_weights)
+  dec_grads = tape.gradient(loss, img_dec.trainable_weights)
+  opt.apply_gradients(zip(enc_grads, img_enc.trainable_weights))
+  opt.apply_gradients(zip(dec_grads, img_dec.trainable_weights))
+
+ return loss.numpy() 
+
+# Text autoencoder training step, starting from embedding
+#
+# x shape: (batch, emb)
+@tf.function
+def txt_dec_enc_train_step(*,
+ opt: tf.keras.optimizers.Optimizer,
+ txt_enc: TextEncoder,
+ txt_dec: TextDecoder,
+ x: tf.Tensor,
+ loss_fn = tf.keras.losses.MeanSquaredError(),
+ train = True,
+):
+ with tf.GradientTape as tape:
+  y = txt_enc(txt_dec(x))
+  loss = loss_fn(x, y)
+
+ if train:
+  enc_grads = tape.gradient(loss, txt_enc.trainable_weights)
+  dec_grads = tape.gradient(loss, txt_dec.trainable_weights)
+  opt.apply_gradients(zip(enc_grads, txt_enc.trainable_weights))
+  opt.apply_gradients(zip(dec_grads, txt_dec.trainable_weights))
+
+ return loss.numpy() 
+
+
+# Image binary operation training step
+#
+# Starts in embedding space, translates to image space, applies binary op, then moves back to embedding space
+#
+# x shape: ((batch, emb), (batch, emb)), (batch, emb)
+@tf.function
+def img_bin_op_train_step(*,
+ opt: tf.keras.optimizers.Optimizer,
+ img_enc: ImgEncoder,
+ img_dec: ImgDecoder,
+ img_bin_op: ImgBinOp,
+ inputs: tuple[tuple[tf.Tensor, tf.Tensor], tf.Tensor],
+ loss_fn = tf.keras.losses.MeanSquaredError(),
+ train = True,
+):
+ (x,y),z = inputs
+ with tf.GradientTape as tape:
+  x_img = img_dec(x)
+  y_img = img_dec(y)
+  z_img_pred = img_bin_op(x_img, y_img)
+  z_pred = img_enc(z_img_pred)
+
+  loss = loss_fn(z, z_pred)
+
+ if train:
+  enc_grads = tape.gradient(loss, img_enc.trainable_weights)
+  dec_grads = tape.gradient(loss, img_dec.trainable_weights)
+  bin_op_grads = tape.gradient(loss, img_bin_op.trainable_weights)
+  opt.apply_gradients(zip(enc_grads, img_enc.trainable_weights))
+  opt.apply_gradients(zip(dec_grads, img_dec.trainable_weights))
+  opt.apply_gradients(zip(bin_op_grads, img_bin_op.trainable_weights))
+
+ return loss.numpy()
+
+# Image binary operation training step
+#
+# Starts in embedding space, translates to text space, applies binary op, then moves back to embedding space
+#
+# x shape: ((batch, emb), (batch, emb)), (batch, emb)
+@tf.function
+def txt_bin_op_train_step(*,
+ opt: tf.keras.optimizers.Optimizer,
+ txt_enc: TxtEncoder,
+ txt_dec: TxtDecoder,
+ txt_bin_op: TxtBinOp,
+ inputs: tuple[tuple[tf.Tensor, tf.Tensor], tf.Tensor],
+ loss_fn = tf.keras.losses.MeanSquaredError(),
+ train = True,
+):
+ (x,y),z = inputs
+ with tf.GradientTape as tape:
+  x_txt = txt_dec(x)
+  y_txt = txt_dec(y)
+  z_txt_pred = txt_bin_op(x_txt, y_txt)
+  z_pred = txt_enc(z_txt_pred)
+
+  loss = loss_fn(z, z_pred)
+
+ if train:
+  enc_grads = tape.gradient(loss, txt_enc.trainable_weights)
+  dec_grads = tape.gradient(loss, txt_dec.trainable_weights)
+  bin_op_grads = tape.gradient(loss, txt_bin_op.trainable_weights)
+  opt.apply_gradients(zip(enc_grads, txt_enc.trainable_weights))
+  opt.apply_gradients(zip(dec_grads, txt_dec.trainable_weights))
+  opt.apply_gradients(zip(bin_op_grads, txt_bin_op.trainable_weights))
+
+ return loss.numpy()
+
+def train(*,
+ opt: tf.keras.optimizers.Optimizer,
+ epochs: int,
+ steps_per_epoch: int,
+ valid_steps_per_epoch: int,
+ img_enc: ImgEncoder,# imb -> emb
+ img_dec: ImgDecoder,# emb -> img
+ txt_enc: TextEncoder,# txt -> emb
+ txt_dec: TextDecoder,# emb -> txt
+ img_bin_op: ImgBinOp,# img ⊕  img -> img 
+ txt_bin_op: TextBinOp,# txt ⊞ txt -> txt
+ emb_dataset: tf.data.Dataset,# infinitely looped, unabeled
+ img_train: tf.data.Dataset,# infinitely looped, unlabeled
+ txt_train: tf.data.Dataset,# infinitely looped, unlabeled
+ img_valid: tf.data.Dataset,# infinitely looped, unlabeled
+ txt_valid: tf.data.Dataset,# infinitely looped, unlabeled
+):
+#FIXME randomize image order
+
+ emb_sum = emb_dataset.zip(emb_dataset).map(lambda a,b: a+b)
+ emb_sum_iter = iter(emb_sum)
+ 
+ emb_iter = iter(emb_dataset)
+ img_train_iter = iter(img_train)
+ txt_train_iter = iter(txt_train)
+ img_valid_iter = iter(img_valid)
+ txt_valid_iter = iter(txt_valid)
+
+ for e in range(epochs):
+  print(f"Epoch {e}/{epochs}")
+
+  losses = defaultdict(list)
+  
+  for s in range(steps_per_epoch):
+   # img enc/dec roundtrip
+   losses['img_enc_dec'].append(img_enc_dec_train_step(
+    opt=opt,
+    img_enc=img_enc,
+    img_dec=img_dec,
+    x=next(img_train_iter),
+   ))
+
+   # text enc/dec roundtrip
+   losses['txt_enc_dec'].append(txt_enc_dec_train_step(
+    opt=opt,
+    txt_enc=txt_enc,
+    txt_dec=txt_dec,
+    x=next(txt_train_iter),
+   ))
+
+   # img dec/enc roundrip (starting from emb)
+   losses['img_dec_enc'].append(img_dec_enc_train_step(
+    opt=opt,
+    img_enc=img_enc,
+    img_dec=img_dec,
+    x=next(img_train_iter),
+   ))
+
+   # text dec/enc roundtrip (starting from emb)
+   losses['txt_dec_enc'].append(txt_dec_enc_train_step(
+    opt=opt,
+    txt_enc=txt_enc,
+    txt_dec=txt_dec,
+    x=next(txt_train_iter),
+   ))
+
+   # img binop roundtrip
+   losses['img_bin_op'].append(img_bin_op_train_step(
+    opt=opt,
+    img_enc=img_enc,
+    img_dec=img_dec,
+    img_bin_op=img_bin_op,
+    x=next(emb_sum_iter),
+   ))
+
+   # text binop roundtrip
+   losses['txt_bin_op'].append(txt_bin_op_train_step(
+    opt=opt,
+    txt_enc=txt_enc,
+    txt_dec=txt_dec,
+    txt_bin_op=txt_bin_op,
+    x=next(emb_sum_iter),
+   ))
+
+   loss_keys = list(sorted(losses.keys()))
+   for k in loss_keys:
+    l = fmean(losses[k])
+    
+    print(f"{k}: {l}")
+
+  valid_losses = defaultdict(list)
+
+  for s in range(valid_steps_per_epoch):
+   # img enc/dec roundtrip
+   valid_losses['img_enc_dec'].append(img_enc_dec_train_step(
+    opt=opt,
+    img_enc=img_enc,
+    img_dec=img_dec,
+    x=next(img_valid_iter),
+    train=False,
+   ))
+
+   # text enc/dec roundtrip
+   valid_losses['txt_enc_dec'].append(txt_enc_dec_train_step(
+    opt=opt,
+    txt_enc=txt_enc,
+    txt_dec=txt_dec,
+    x=next(txt_valid_iter),
+    train=False,
+   ))
+
+   # img dec/enc roundrip (starting from emb)
+   valid_losses['img_dec_enc'].append(img_dec_enc_train_step(
+    opt=opt,
+    img_enc=img_enc,
+    img_dec=img_dec,
+    x=next(img_valid_iter),
+    train=False,
+   ))
+
+   # text dec/enc roundtrip (starting from emb)
+   valid_losses['txt_dec_enc'].append(txt_dec_enc_train_step(
+    opt=opt,
+    txt_enc=txt_enc,
+    txt_dec=txt_dec,
+    x=next(txt_valid_iter),
+    train=False,
+   ))
+
+   # img binop roundtrip
+   valid_losses['img_bin_op'].append(img_bin_op_train_step(
+    opt=opt,
+    img_enc=img_enc,
+    img_dec=img_dec,
+    img_bin_op=img_bin_op,
+    x=next(emb_sum_iter),
+    train=False,
+   ))
+
+   # text binop roundtrip
+   valid_losses['txt_bin_op'].append(txt_bin_op_train_step(
+    opt=opt,
+    txt_enc=txt_enc,
+    txt_dec=txt_dec,
+    txt_bin_op=txt_bin_op,
+    x=next(emb_sum_iter),
+    train=False,
+   ))
+
+   valid_loss_keys = list(sorted(valid_losses.keys()))
+   for k in valid_loss_keys:
+    l = fmean(valid_losses[k])
+    
+    print(f"valid {k}: {l}")
+
+
+def _gen_emb_dataset(*, batch: int, emb: int, dtype=tf.float32):
+ while True:
+  yield tf.random.uniform((batch, emb), dtype=dtype)
+
+def emb_dataset(*, batch: int, emb: int, dtype=tf.float32):
+ gen = lambda: _gen_emb_dataset(batch=batch, emb=emb, dtype=dtype)
+
+ return tf.data.Dataset.from_generator(gen, output_signature=tf.TensorSpec(shape=(batch,emb,), dtype=type))
+
 
 if __name__=="__main__":
-  img0_train, img0_valid = load_img_datasets()
-  text0_train, text0_valid, grapheme_count = load_text_datasets()
-  img1_train, img1_valid = load_img_datasets()
-  text1_train, text1_valid, grapheme_count = load_text_datasets()
+  img_train, img_valid = load_img_datasets()
+  txt_train, txt_valid, grapheme_count = load_text_datasets()
 
-  train = tf.data.Dataset.zip(img0_train, text0_train, img1_train, text1_train)
-  valid = tf.data.Dataset.zip(img0_valid, text0_valid, img1_valid, text1_valid)
+  img_train = img_train.repeat()
+  img_valid = img_train.repeat()
+  txt_train = txt_train.repeat()
+  txt_valid = txt_valid.repeat()
+
+  # img0_train, img0_valid = load_img_datasets()
+  # text0_train, text0_valid, grapheme_count = load_text_datasets()
+  # img1_train, img1_valid = load_img_datasets()
+  # text1_train, text1_valid, grapheme_count = load_text_datasets()
+
+  # train = tf.data.Dataset.zip(img0_train, text0_train, img1_train, text1_train)
+  # valid = tf.data.Dataset.zip(img0_valid, text0_valid, img1_valid, text1_valid)
 
   num_heads = 8
-  m = EmbeddingRoundTripper(num_heads=num_heads, vocab_size=grapheme_count)
+  # m = EmbeddingRoundTripper(num_heads=num_heads, vocab_size=grapheme_count)
+  emb = 128
 
-  m.compile(
-   loss='categorical_crossentropy',
-   optimizer='adam',
-   run_eagerly=True,
-  )
 
-  m.fit(
-   train,
+  # m.compile(
+  #  loss='categorical_crossentropy',
+  #  optimizer='adam',
+  #  run_eagerly=True,
+  # )
+
+  # m.fit(
+  #  train,
+  #  epochs=20,
+  #  validation_data=valid
+  # )
+
+  opt = Adam()
+  img_enc = ImgEncoder(emb=emb)
+  img_dec = ImgDecoder(emb=emb, w=W, h=H)
+  txt_enc = TextEncoder(num_heads=num_heads, input_vocab_size=grapheme_count, emb=emb)
+  txt_dec = TextDecoder(num_heads=num_heads, emb=emb, output_vocab_size=grapheme_count)
+
+  emb_dataset_ = emb_dataset(batch=BATCH, emb=emb)
+
+ # opt: tf.keras.optimizers.Optimizer,
+ # epochs: int,
+ # steps_per_epoch: int,
+ # img_enc: ImgEncoder,# imb -> emb
+ # img_dec: ImgDecoder,# emb -> img
+ # txt_enc: TextEncoder,# txt -> emb
+ # txt_dec: TextDecoder,# emb -> txt
+ # img_bin_op: ImgBinOp,# img ⊕  img -> img 
+ # txt_bin_op: TextBinOp,# txt ⊞ txt -> txt
+ # emb_dataset: tf.data.Dataset,# infinitely looped, unabeled
+ # img_train: tf.data.Dataset,# infinitely looped, unlabeled
+ # txt_train: tf.data.Dataset,# infinitely looped, unlabeled
+ # img_valid: tf.data.Dataset,# infinitely looped, unlabeled
+ # txt_valid: tf.data.Dataset,# infinitely looped, unlabeled
+  train(
+   opt=opt,
    epochs=20,
-   validation_data=valid
+   steps_per_epoch=1000,
+   valid_steps_per_epoch=100,
+   img_enc=img_enc,
+   img_dec=img_dec,
+   txt_enc=txt_enc,
+   txt_dec=txt_dec,
+   emb_dataset=emb_dataset_,
+   img_train=img_train,
+   txt_train=txt_train,
+   img_valid=img_valid,
+   txt_valid=txt_valid,
   )
